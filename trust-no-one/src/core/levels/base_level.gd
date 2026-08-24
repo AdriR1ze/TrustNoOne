9278e818d5893ad45c9095548cc66eac4ef81b5f
AdriR1ze <138025664+AdriR1ze@users.noreply.github.com>
2026-08-24 18:46:05 -0300

    3d

--- trust-no-one/src/core/levels/base_level.gd
+++ trust-no-one/src/core/levels/base_level.gd
@@ -1,8 +1,8 @@
 extends Node3D
 class_name BaseLevel
 
-func get_default_player_spawn() -> Vector2:
-	var spawner := get_node_or_null("PlayerSpawner") as Node2D
+func get_default_player_spawn() -> Vector3:
+	var spawner := get_node_or_null("PlayerSpawner") as Node3D
 	if spawner != null:
 		return spawner.global_position
-	return Vector2(10, 10)
+	return Vector3(10, 10, 10)
